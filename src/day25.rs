db6ccaa7c4eb8d6b72e65ab25931eed9ec93efc3
Michele Abbrescia <mikiab84@gmail.com>
2024-12-26 18:16:30 +0100

    Day 25: Refactoring

--- src/day25.rs
+++ src/day25.rs
@@ -1,5 +1,3 @@
-use core::str;
-
 use aoc_runner_derive::aoc;
 use smallvec::SmallVec;
 
@@ -9,27 +7,17 @@ pub fn part1(input: &str) -> u32 {
     let mut keys = SmallVec::<[&[u8]; 64]>::new();
 
     for (i, kind) in input.bytes().enumerate().step_by((6 * 7) + 1) {
-        match kind {
-            b'#' => {
-                //println!("lock at {i}");
-                let lock = &input.as_bytes()[i..i + (6 * 7)];
-                // println!("{lock:?}");
-                locks.push(lock);
-            }
-            b'.' => {
-                //println!("key at {i}");
-                let key = &input.as_bytes()[i..usize::min(i + (6 * 7), input.len())];
-                // println!("{key:?}");
-                keys.push(key);
-            }
-            _ => {}
+        let item = &input.as_bytes()[i..usize::min(i + (6 * 7), input.len())];
+        if kind == b'#' {
+            locks.push(item);
+        } else {
+            keys.push(item);
         }
     }
 
-    let mut no_overlap = 0;
-    for lock in locks.iter() {
-        for key in keys.iter() {
-            //println!("Trying key {} with lock {}", k, l);
+    let mut no_overlaps = 0;
+    for lock in &locks {
+        for key in &keys {
             let pins = [
                 6, 12, 18, 24, 30, 7, 13, 19, 25, 31, 8, 14, 20, 26, 32, 9, 15, 21, 27, 33, 10, 16,
                 22, 28, 34,
@@ -41,12 +29,12 @@ pub fn part1(input: &str) -> u32 {
                 .count();
 
             if overlaps == 0 {
-                no_overlap += 1;
+                no_overlaps += 1;
             }
         }
     }
 
-    no_overlap
+    no_overlaps
 }
 
 #[cfg(test)]
